3ad67642cd0def29e00c70d207e99232047d2ab4
davidw <n30r3b3l@gmail.com>
2015-08-25 19:03:34 -0500

    Final Commit

---
+++ app/src/androidTest/java/com/davidwestberry/ribbit/ui/SignUpActivityTest.java
@@ -0,0 +1,24 @@
+package com.davidwestberry.ribbit.ui;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by DavidW on 4/9/2015.
+ */
+public class SignUpActivityTest extends ActivityInstrumentationTestCase2<SignUpActivity> {
+
+    public SignUpActivityTest(Class activityClass) {
+        super(activityClass);
+    }
+
+    public void setUp() throws Exception {
+        super.setUp();
+
+    }
+
+    public void testOnCreate() throws Exception {
+
+    }
+}
